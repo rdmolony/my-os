1489df312a8d9ee706aa5b0e3056c19c392450a2
Rowan Molony <rowanmolony@gmail.com>
2024-09-25 10:09:44 +0100

    fix: nixpkgs-unfree-unstable uses 24.05

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixpkgs-unfree-unstable.url = "github:numtide/nixpkgs-unfree";
+    nixpkgs-unstable.url = "github:numtide/nixpkgs-unfree/nixos-unstable";
   };
 
   outputs = {
--- users/rowanm/home.nix
+++ users/rowanm/home.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  vscode-unstable = inputs.nixpkgs-unfree-unstable.legacyPackages.${pkgs.system}.vscode;
+  vscode-unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.vscode;
 in
 {
   # Home Manager needs a bit of information about you and the paths it should
