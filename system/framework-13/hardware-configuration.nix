ac02980822a04799c5c503426b80cb3819d59efb
Rowan Molony <rowanmolony@gmail.com>
2025-05-29 12:15:10 +0100

    fix: /boot permissions
    Prior permissions were too permissive, and
    exposed a security hole - caught by the
    nix builder on adding tailscale in my
    last commit

--- system/framework-13/hardware-configuration.nix
+++ system/framework-13/hardware-configuration.nix
@@ -21,7 +21,7 @@
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/7EE8-1588";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices = [ ];
